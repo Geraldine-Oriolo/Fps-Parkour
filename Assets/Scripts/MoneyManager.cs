97196efede72bad134aeb7ec084739a72ff193aa
Geraldine-Oriolo <49374998@est.ort.edu.ar>
2025-07-09 18:04:17 -0300

    Tp

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIManager uiManager;
+
+    public bool UpdateMoney(float amount)
+    {
+        if (money + amount >= 0)
+        {
+            money += amount;
+            uiManager.UpdateMoneyText(money.ToString());
+            return true;
+        }
+        return false;
+    }
+}
+
---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyScript : MonoBehaviour
+{
+    public float cost;
+    public MoneyManager moneyManager;
+
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (moneyManager.UpdateMoney(-cost))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
